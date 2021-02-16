8a8cf497224144ad80a167aaf8a5d9798250f9ab
sudolorian <suad.galijasevic@edu.fit.ba>
2021-02-16 16:07:11 +0100

    Model folder #10

---
+++ webapp/backend-api/achievements-data/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace achievements_data.Models
+{
+    class Person
+    {
+    }
+}
--- webapp/backend-api/achievements-data/Person.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace achievements_data
-{
-    public class Person
-    {
-        
-    }
-}
