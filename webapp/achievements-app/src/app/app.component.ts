3252b2a4cdedcb23b7d3294376bc83b15f1e86ef
mihigs <miralem.avdic24@gmail.com>
2022-06-27 14:06:00 +0200

    Added Angular boilerplate with preset modules #31

--- webapp/achievements-app/src/app/app.component.ts
+++ webapp/achievements-app/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'achievements-app';
+
+    constructor(private translateService: TranslateService) {
+        translateService.addLangs(['tr', 'en']);
+        const browserLanguage = translateService.getBrowserLang();
+        translateService.use(browserLanguage.match(/en|tr/) ? browserLanguage : 'en');
+    }
+
 }
--- webapp/achievements-app/src/environments/environment.prod.ts
+++ webapp/achievements-app/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: 'http://prod-locahost:8080'
 };
