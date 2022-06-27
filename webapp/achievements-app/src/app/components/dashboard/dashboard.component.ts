6351013bc595c99159ca0ba7a03ba2d06e4810f1
mihigs <miralem.avdic24@gmail.com>
2022-06-27 21:11:23 +0200

    Initialized pages for all features #37

--- webapp/achievements-app/src/app/components/dashboard/dashboard.component.ts
+++ webapp/achievements-app/src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthenticationService} from '../../services/authentication.service';
+import {Router} from '@angular/router';
 
 export interface Tile {
     color: string;
@@ -7,6 +8,7 @@ export interface Tile {
     rows: number;
     name: string;
     description: string;
+    path: string;
 }
 
 @Component({
@@ -16,19 +18,22 @@ export interface Tile {
 })
 export class DashboardComponent implements OnInit {
 
-    constructor(private authenticationService: AuthenticationService) {}
+    constructor(private authenticationService: AuthenticationService, private router: Router) {}
 
     ngOnInit(): void {}
 
     tiles: Tile[] = [
-        {cols: 2, rows: 3, color: '#F46036', name: "Achievements", description: "Lorem ipsumn"},
-        {cols: 2, rows: 3, color: '#1B998B', name: "Teams", description: "Lorem ipsumn"},
-        {cols: 2, rows: 3, color: '#E71D36', name: "Events", description: "Lorem ipsumn"},
-        {cols: 2, rows: 3, color: '#2E294E', name: "Members", description: "Lorem ipsumn"},
+        {cols: 2, rows: 3, color: '#F46036', name: "Achievements", description: "Lorem ipsumn", path: "achievements"},
+        {cols: 2, rows: 3, color: '#1B998B', name: "Teams", description: "Lorem ipsumn", path: "teams"},
+        {cols: 2, rows: 3, color: '#E71D36', name: "Events", description: "Lorem ipsumn", path: "events"},
+        {cols: 2, rows: 3, color: '#2E294E', name: "Members", description: "Lorem ipsumn", path: "members"},
     ];
 
     logout() {
         this.authenticationService.logout();
     }
 
+    navigateTo(path: string) {
+        this.router.navigate([path]);
+    }
 }
