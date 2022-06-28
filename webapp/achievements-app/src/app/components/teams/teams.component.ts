9973a1c0535935639995db89c7f43f3488680524
mihigs <miralem.avdic24@gmail.com>
2022-06-28 13:21:18 +0200

    Display teams #43

--- webapp/achievements-app/src/app/components/teams/teams.component.ts
+++ webapp/achievements-app/src/app/components/teams/teams.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { CreateTeamModalComponent } from './create-team-modal/create-team-modal.component';
+import { Team } from 'src/app/interfaces/Teams/Team';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-teams',
@@ -10,10 +12,13 @@ import { CreateTeamModalComponent } from './create-team-modal/create-team-modal.
 })
 export class TeamsComponent implements OnInit {
 
-  constructor(private router: Router, public dialog: MatDialog) { }
+  constructor(private router: Router, public dialog: MatDialog, private apiService: ApiService) { }
 
   ngOnInit(): void {
+    this.getTeams();
   }
+
+  teams: Team[];
 
   goBack(): void {
     this.router.navigate([".."]);
@@ -27,6 +32,12 @@ export class TeamsComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(result => {
     });
+  }
+
+  getTeams(): void{
+    this.apiService.get("/Team/list").subscribe((resp: any) => {
+      this.teams = resp.result;
+    })
   }
 
 }
---
+++ webapp/achievements-app/src/app/interfaces/Teams/Team.ts
@@ -0,0 +1,9 @@
+export class Team{
+    id: number
+    name : string
+    description : string
+    availableAchievements: string[]
+    created: Date
+    createdBy: string
+    teamMembers: string[]
+}
