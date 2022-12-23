7865581cd0d011dd3767d8ff101b92e97cd88035
mihigs <miralem.avdic24@gmail.com>
2022-12-23 14:43:36 +0100

    Added a switch for language change #68

--- webapp/achievements-app/src/app/components/dashboard/dashboard.component.ts
+++ webapp/achievements-app/src/app/components/dashboard/dashboard.component.ts
@@ -1,13 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthenticationService} from '../../services/authentication.service';
 import {Router} from '@angular/router';
+import {TranslateService} from '@ngx-translate/core';
 
 export interface Tile {
     color: string;
     cols: number;
     rows: number;
     name: string;
-    description: string;
     path: string;
 }
 
@@ -17,16 +17,16 @@ export interface Tile {
     styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-    constructor(private authenticationService: AuthenticationService, private router: Router) {}
+    language_selected: String = this.translateService.currentLang;
+    constructor(private authenticationService: AuthenticationService, private router: Router, private translateService: TranslateService) {}
 
     ngOnInit(): void {}
 
     tiles: Tile[] = [
-        {cols: 2, rows: 3, color: '#F46036', name: "Achievements", description: "Lorem ipsumn", path: "achievements"},
-        {cols: 2, rows: 3, color: '#1B998B', name: "Teams", description: "Lorem ipsumn", path: "teams"},
-        {cols: 2, rows: 3, color: '#E71D36', name: "Events", description: "Lorem ipsumn", path: "events"},
-        {cols: 2, rows: 3, color: '#2E294E', name: "Members", description: "Lorem ipsumn", path: "members"},
+        {cols: 2, rows: 3, color: '#F46036', name: "Achievements", path: "achievements"},
+        {cols: 2, rows: 3, color: '#1B998B', name: "Teams", path: "teams"},
+        {cols: 2, rows: 3, color: '#E71D36', name: "Events", path: "events"},
+        {cols: 2, rows: 3, color: '#2E294E', name: "Members", path: "members"},
     ];
 
     logout() {
@@ -35,5 +35,9 @@ export class DashboardComponent implements OnInit {
 
     navigateTo(path: string) {
         this.router.navigate([path]);
+    }
+
+    switchLanguage(lang: string){
+        this.translateService.use(lang);
     }
 }
